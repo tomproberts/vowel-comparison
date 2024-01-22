94f13cb620df3f33d7b49e34ace23d33fb777d52
Thomas Paul Roberts <tomproberts@outlook.com>
2024-01-22 17:47:04 +0000

    Added elipses

--- Plot_all_monophthongs.R
+++ Plot_all_monophthongs.R
@@ -9,6 +9,8 @@ my_vowels <- read.csv("./formants.csv", sep = ";", nrows = 0) %>%
 p <- my_vowels %>%
   ggplot(.)+
   aes(x = f2, y = f1, color = phon, label = phon)+
+  stat_ellipse(type = "t", level = 0.67, linetype = 2, linewidth = 0.75,
+               geom = "polygon", alpha = 0.05, aes(fill = phon))+
   theme_classic()+
   theme(legend.position = "none")+
   scale_x_reverse(position = "top", name = "F2 (Hz)")+
