49a570c2ba15b8f51f75a2f029c65fc8760af803
Thomas Paul Roberts <tomproberts@outlook.com>
2024-01-23 20:33:35 +0000

    Filter out if liquids or nasals follow the vowel

--- Plot_all_monophthongs.R
+++ Plot_all_monophthongs.R
@@ -2,9 +2,18 @@ library(dplyr)
 library(ggplot2)
 
 
+MONOPHTHONGS <- c("iː", "ɪ", "a", "aː", "ə", "ɛ", "eː", "ɐ", 
+                  "ɔ", "oː", "ʊ", "uː", "œ", "øː", "ʏ", "yː")
+NASALS <- c("n", "n̩", "m", "m̩", "ɲ", "ŋ")
+LIQUIDS <- c("l", "l̩", "ʁ")
+
 # Read CSV
 my_vowels <- read.csv("./formants.csv", sep = ";", nrows = 0) %>%
-  filter(phon %in% c("iː", "ɪ", "a", "aː", "ə", "ɛ", "eː", "ɐ", "ɔ", "oː", "ʊ", "uː"))
+  filter(phon %in% MONOPHTHONGS, !(next_phon %in% c(NASALS, LIQUIDS)))
+
+# Calculate bark formant values
+my_vowels$b1 <- eval(to_bark(my_vowels$f1), my_vowels)
+my_vowels$b2 <- eval(to_bark(my_vowels$f2), my_vowels)
 
 # Plot
 p <- my_vowels %>%
@@ -17,8 +26,12 @@ p <- my_vowels %>%
   scale_x_reverse(position = "top", name = "F2 (Hz)")+
   scale_y_reverse(position = "right", name = "F1 (Hz)")+
   geom_text(hjust=0, vjust=0, size = 3)+
-  ggtitle("Vowel Space of Clone")
+  ggtitle("Vowel Space of Clone, Monophthongs, \"Die Sonne und der Wind\"")
 
 # Display plot
-ggsave("ggplot.pdf", device=cairo_pdf)
+# ggsave("ggplot.pdf", device=cairo_pdf)
 print(p)
+
+to_bark <- function(f) {
+  return (13 * atan(0.00076 * f) + 3.5 * atan((f / 7500) ** 2))
+}
